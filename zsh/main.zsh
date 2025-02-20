525a60600f6a3cc75f7f339b6f8baa2adbd11fe5
leninner <mazabandalenin180@gmail.com>
2025-02-20 14:58:59 -0500

    chote: update config

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -42,3 +42,6 @@ export PATH=$MAVEN_HOME/bin:$PATH
 
 # Load Powerlevel10k theme configuration
 [[ ! -f ~/.p10k.zsh ]] || source $HOME/.p10k.zsh
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
